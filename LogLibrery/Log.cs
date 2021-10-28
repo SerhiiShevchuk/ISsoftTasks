bc14296b6e002cfa061e5c08de7753e191f564ac
SerhiiShevchuk <SerhiiShevchuk@coherentsolutions.com>
2021-10-28 03:41:51 +0300

    NET02.3

---
+++ LogLibrery/Log.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Listener;
+
+namespace Log
+{
+    public class Log : ILog
+    {
+        public string Name { get; set; }
+       // public ILevel Threshold { get; set; }
+        public List<IListener> Listener { get; set; } = new List<IListener>();
+
+        public Log(LogSettings settings)
+        {
+            
+        }
+
+        public void LoadListeners()
+        {
+
+        }
+    }
+}
---
+++ LogLibrery/LogJsonRepo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Log
+{
+    class LogJsonRepo : ILogRepo
+    {
+        public LogSettings LoadSettings()
+        {
+            using (FileStream json = new FileStream(_path, FileMode.OpenOrCreate))
+            {
+
+                return JsonSerializer.DeserializeAsync<LogSettings>(json).Result;
+            }
+        }
+
+        private string _path = @"..\..\..\Config\LogSettings.json";
+    }
+}
